7aa8ab92ee06630622c8611219841459ca9eb9b4
joeylouise <joey.scott@acuris.com>
2018-03-14 14:37:13 +0000

    Add document3.json and document4.json

--- nested-dominantSector-subsectors/index-documents.sh
+++ nested-dominantSector-subsectors/index-documents.sh
@@ -3,6 +3,6 @@
 INDEX='my-first-index'
 TYPE='intelligence'
 
-for file in document1.json document2.json; do
+for file in document1.json document2.json document3.json document4.json; do
   curl -XPOST -d@"$file" "http://localhost:9200/$INDEX/$TYPE/"
 done
