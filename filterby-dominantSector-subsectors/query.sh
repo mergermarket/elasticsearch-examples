e7ce2ee80a6639e11da4760d51041b6ec817ff1a
Fahri Cavusoglu <fahric@hotmail.co.uk>
2018-03-19 12:27:45 +0000

    updated docker image to match serach service,added new scenario for filter

---
+++ filterby-dominantSector-subsectors/query.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+INDEX='my-first-index'
+TYPE='intelligence'
+
+if [ -z "$1" ]
+  then
+    echo "Usage: $0 <query json file>"
+    exit 1
+fi
+
+QUERYJSON="$1"
+
+set -x
+curl --user elastic:changeme -XPOST -d@"$QUERYJSON" "http://localhost:9200/$INDEX/_search?pretty"
+
+
